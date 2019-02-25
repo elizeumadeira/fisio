970436bbd25414a7055324456ddadcde5708eee1
Elizeu Santos Madeira <elizeu.madeira@gmail.com>
2019-02-24 22:59:16 -0300

    Incluído cabeçalho Authorization

--- app/Http/Middleware/ModifyHeadersMiddleware.php
+++ app/Http/Middleware/ModifyHeadersMiddleware.php
@@ -18,7 +18,7 @@ class ModifyHeadersMiddleware
         $response = $next($request);
         $response->header('Access-Control-Allow-Origin', '*');
         $response->header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PUSH, PATCH, DELETE');
-        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type');
+        $response->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
         return $response;
     }
 }
